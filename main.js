63b59bd5311a248f89dd8c7b98b22371514e5cf4
Otto Pecz <otto.pecz@gmail.com>
2013-12-29 20:59:28 +0100

    Example for simple class inheritance

---
+++ main.js
@@ -0,0 +1,34 @@
+Function.prototype.inheritsFrom = function (SuperClass) {
+    var Dummy = function () {
+        this.constructor = this;
+    };
+
+    Dummy.prototype = SuperClass.prototype;
+
+    this.prototype = new Dummy();
+
+    this.prototype.__super__ = SuperClass.prototype;
+
+    return this;
+};
+
+var SuperClass = function () {
+    this.superOne = 1;
+};
+
+SuperClass.prototype.superTwo = function () {
+    return this.superOne;
+};
+
+var SubClass = function () {
+    this.subOne = 3;
+}.inheritsFrom(SuperClass);
+
+SubClass.prototype.subTwo = function () {
+    debugger;
+    return this.subOne;
+};
+
+var subObj = new SubClass();
+
+debugger;
